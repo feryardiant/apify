58ac6e7a954657c76010ebd0ffbf8a2b54eefd18
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-03-03 23:03:32 +0700

    Call response.end()
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- trynow.js
+++ trynow.js
@@ -1,5 +1,9 @@
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
 module.exports = async (req, res) => {
   const paths = req.url.split('/')
 
-  return { paths }
+  res.end(JSON.stringify({paths}))
 }
